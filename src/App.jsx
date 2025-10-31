a4e948cb4649df7069894e60aaf78f566b5ceb67
Kunal Ojha <87754559+kunalojha69@users.noreply.github.com>
2025-10-31 21:54:36 +0530

    complete login page

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+import SignUpPage from './pages/SignUpPage';
+import LoginPage from './pages/LoginPage';
+import ProfilePage from './pages/ProfilePage';
+import SettingsPage from './pages/SettingsPage';
+import { useAuthStore } from './store/useAuthStore.js';
+import { useEffect } from 'react';
+import { LoaderCircle } from 'lucide-react';
+import { Toaster } from 'react-hot-toast';
+
+const App = () => {
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log({ authUser });
+
+  if (isCheckingAuth && !authUser) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <LoaderCircle className="size-10 animate-spin" />
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route
+          path="/profile"
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+        />
+      </Routes>
+      <Toaster />
+    </div>
+  );
+};
+export default App;
---
+++ src/store/useAuthStore.js
@@ -0,0 +1,60 @@
+import { create } from 'zustand';
+import { axiosInstance } from '../lib/axios.js';
+import toast from 'react-hot-toast';
+
+export const useAuthStore = create((set) => ({
+  authUser: null,
+  isSigningUp: false,
+  isLoggingIn: false,
+  isUpdatingProfile: false,
+
+  isCheckingAuth: true,
+
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get('/auth/check');
+      set({ authUser: res.data });
+    } catch (error) {
+      console.error(`Error in authentication: ${error}`);
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+
+  signup: async (data) => {
+    try {
+      set({ isSigningUp: true });
+      const res = await axiosInstance.post('auth/signup', data);
+      set({ authUser: res.data });
+      toast.success('Account Created Successfully');
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isSigningUp: false });
+    }
+  },
+
+  login: async (data) => {
+    try {
+      set({ isLoggingIn: true });
+      const res = await axiosInstance.post('auth/login', data);
+      set({ authUser: res.data });
+      toast.success('Logged in successfully');
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isLoggingIn: false });
+    }
+  },
+
+  logout: async () => {
+    try {
+      await axiosInstance.post('/auth/logout');
+      set({ authUser: null });
+      toast.success('Logged out successfully');
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  },
+}));
